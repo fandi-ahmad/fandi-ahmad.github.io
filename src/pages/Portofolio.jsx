711e7538687aad75a2555e95c5a4514823f89f4c
fandi-ahmad <fandi4160@gmail.com>
2023-08-07 21:13:05 +0800

    testing modified file

---
+++ src/pages/Portofolio.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react'
+
+const Portofolio = () => {
+
+  useEffect(() => {
+    console.log('ini halaman portofolio');
+  }, [])
+
+  return (
+    <div>
+      <h1>Portofolio</h1>
+    </div>
+  )
+}
+
+export default Portofolio

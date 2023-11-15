c9cb661d86b12bcfb875bb1b0304460ae005e60a
fandi-ahmad <fandi4160@gmail.com>
2023-11-15 15:53:05 +0800

    fix conflict

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-<<<<<<< HEAD
-=======
 import './assets/tailwind.css'
 import './assets/fontawesome/css/all.css'
->>>>>>> e6fe9ea5caf1447e0d9b774b927bbd4871a50cf9
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

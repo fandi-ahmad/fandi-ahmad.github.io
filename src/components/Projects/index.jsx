8ce17531ef162fc3ae584a723e47441a8826129a
fandi-ahmad <fandi4160@gmail.com>
2024-07-03 22:11:07 +0800

    update project image and modified category

--- src/components/Projects/index.jsx
+++
@@ -1,9 +0,0 @@
-import PenaWeb from "./PenaWeb";
-import HoneyProduction from "./HoneyProduction";
-import WhackaMole from "./WhackaMole";
-import Todolist from "./Todolist";
-import WeatherApp from "./WeatherApp";
-import SipsApp from "./SipsApp";
-import PersonalWeb from "./PersonalWeb";
-
-export { PenaWeb, HoneyProduction, WhackaMole, Todolist, WeatherApp, SipsApp, PersonalWeb }
